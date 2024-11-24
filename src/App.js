c02da5107b1a93f7a07ffb6d0326b94d8c2dc12b
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-24 07:15:09 +0300

    #1 - Add an "About Me" section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import ThreeScene from './components/ThreeScene';
 import Contact from './components/contact/Contact';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
+import About from './components/about/About';
 function App() {
   return (
     <div className="App">
       {/* <ThreeScene /> */}
       <Hero />
+      <About />
       {/* <Experience />
       <Contact /> */}
     </div>
