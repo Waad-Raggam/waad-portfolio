ad9927438f9ebf29a552c40f5d3523a461d244df
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-24 09:49:29 +0300

    #1 - Implement "Experience" section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import About from './components/About';
+import Experience from './components/Experience';
 function App() {
   return (
     <div className="App">
       {/* <ThreeScene /> */}
       <Hero />
       <About />
-      {/* <Experience />
-      <Contact /> */}
+      <Experience />
+      {/* <Contact /> */}
     </div>
   );
 }
