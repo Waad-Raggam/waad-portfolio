72ab7946b4f3c23da12f088340d6e350cdcc0d01
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-24 09:51:25 +0300

    #1 - Implement "Projects" section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hero from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
       {/* <Contact /> */}
     </div>
   );
