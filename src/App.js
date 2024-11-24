03acacb24ab06bc4a7d57d6d64c55a5223c6c8f9
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-24 10:07:08 +0300

    #1 - Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import { StarsCanvas } from "./components/canvas";
 import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+    <BrowserRouter>
+      <div className="App">
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
         </div>
+        <About />
+        <Experience />
+        <Projects />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
-      <About />
-      <Experience />
-      <Projects />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
