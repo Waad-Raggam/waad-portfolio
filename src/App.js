f02ae993d02ed6e032bdd52c41ccfa13d3413272
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-11-24 09:03:25 +0300

    #1 - Fix imports and add canvas, hoc, and utils folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-import ThreeScene from './components/ThreeScene';
-import Contact from './components/contact/Contact';
 import Hero from './components/Hero';
-import Experience from './components/Experience';
-import About from './components/about/About';
 function App() {
   return (
     <div className="App">
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../style";
-import { ComputersCanvas } from "../components/canvas/Computers";
+import { ComputersCanvas } from "../components/canvas/";
 
 const Hero = () => {
   return (
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
